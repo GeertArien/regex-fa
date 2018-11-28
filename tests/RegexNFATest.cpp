980eaf20622e3b2f107941cff345449cd116e02d
Geert Arien <geert@twikit.com>
2018-11-28 20:08:05 +0100

    Add unit tests for all currently supported operators

--- tests/RegexNFATest.cpp
+++ tests/RegexNFATest.cpp
@@ -17,3 +17,39 @@ TEST_CASE( "Regex concatenation", "[RegexNFA]" ) {
 	REQUIRE(!nfa.Match("aaabbcdd"));
 	REQUIRE(nfa.Match("aaabcda"));
 }
+
+TEST_CASE( "Regex 'or' operator", "[RegexNFA]" ) {
+	RegexFA::RegexNFA nfa = PostfixToNFA("abc|.a.");
+	REQUIRE(nfa.Match("aba"));
+	REQUIRE(nfa.Match("aca"));
+	REQUIRE(!nfa.Match("ada"));
+	REQUIRE(!nfa.Match("abca"));
+	REQUIRE(!nfa.Match("ab"));
+}
+
+TEST_CASE( "Regex 'match zero or one' operator", "[RegexNFA]" ) {
+	RegexFA::RegexNFA nfa = PostfixToNFA("ab?.c.");
+	REQUIRE(nfa.Match("abc"));
+	REQUIRE(nfa.Match("ac"));
+	REQUIRE(!nfa.Match("ab"));
+	REQUIRE(!nfa.Match("abbc"));
+	REQUIRE(!nfa.Match("bc"));
+}
+
+TEST_CASE( "Regex 'match zero or more' operator", "[RegexNFA]" ) {
+	RegexFA::RegexNFA nfa = PostfixToNFA("ab*.c.");
+	REQUIRE(nfa.Match("abc"));
+	REQUIRE(nfa.Match("ac"));
+	REQUIRE(nfa.Match("abbc"));
+	REQUIRE(!nfa.Match("ab"));
+	REQUIRE(!nfa.Match("bc"));
+}
+
+TEST_CASE( "Regex 'match one or more' operator", "[RegexNFA]" ) {
+	RegexFA::RegexNFA nfa = PostfixToNFA("ab+.c.");
+	REQUIRE(nfa.Match("abc"));
+	REQUIRE(!nfa.Match("ac"));
+	REQUIRE(nfa.Match("abbc"));
+	REQUIRE(!nfa.Match("ab"));
+	REQUIRE(!nfa.Match("bc"));
+}
